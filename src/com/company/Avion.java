a480492725c70dbfa263703dcf5dfa50726cd81f
GeronimoOrofino <67095868+GeronimoOrofino@users.noreply.github.com>
2020-06-19 00:40:58 -0300

    Update Avion.java

--- src/com/company/Avion.java
+++ src/com/company/Avion.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public abstract class Avion {
+public class Avion {
     protected int capacidad_de_combustible;
     protected int costo_km; //COSTO POR KILOMETRO
     protected int capacidad_de_pasajeros; //CAPACIDAD MAXIMA DE ESTE AVION
-    protected Double velocidad_maxima; //VELOCIDAD EN KM/H
+    protected double velocidad_maxima; //VELOCIDAD EN KM/H
     protected  Propulsion propulsion; //TIPO DE PROPULSION DEL AVION
 }
